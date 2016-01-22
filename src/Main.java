41cab804bfb2f0a2de79b3ab1704b8ac4b450f92
Prakash K <kprakash19940625@gmail.com>
2016-01-22 13:39:47 +0530

    Create Main.java

---
+++ src/Main.java
@@ -0,0 +1,41 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Main {
+
+	@Test
+        public void test1() {
+                GenerateRandom test = new GenerateRandom();
+                int output = test.generate();
+                assertTrue(3 < output);
+        }
+        
+        @Test
+        public void test2(){
+                GenerateRandom test = new GenerateRandom();
+                int output = test.generate();
+                assertTrue(5 < output);
+        }
+        
+        @Test
+        public void test3() {
+                GenerateRandom test = new GenerateRandom();
+                int output = test.generate();
+                assertTrue(5 < output);
+        }
+        
+        @Test
+        public void test4(){
+                GenerateRandom test = new GenerateRandom();
+                int output = test.generate();
+                assertTrue(5 < output);
+        }
+        
+        @Test
+        public void test5(){
+                GenerateRandom test = new GenerateRandom();
+                int output = test.generate();
+                assertTrue(5 < output);
+        }
+}
