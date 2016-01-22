724549c9ad5808efb2d736688e0831fa5769a4d5
Prakash K <kprakash19940625@gmail.com>
2016-01-22 15:15:50 +0530

    Update TestRunner.java

--- src/TestRunner.java
+++ src/TestRunner.java
@@ -15,7 +15,7 @@ public class TestRunner {
       if (failed>0) {
          System.out.println("Failed Cases : \n\t "+result.getFailures());
       }
-      if (failed>=0) {
+      if (failed>0) {
         throw new EmptyStackException();
       }
    }
