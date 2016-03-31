cda98885ab3aa8ec348ae8bb9ace846ecd91fda4
Prakash K <kprakash19940625@gmail.com>
2016-03-31 12:41:00 +0530

    Update GenerateRandom.java

--- src/GenerateRandom.java
+++ src/GenerateRandom.java
@@ -2,7 +2,7 @@ import java.util.Random;
 public class GenerateRandom {
   public int generate() {
     Random randomGenerator = new Random();
-    int n = randomGenerator.nextInt(25);
+    int n = randomGenerator.nextInt(15);
     return n;
   }
 }
